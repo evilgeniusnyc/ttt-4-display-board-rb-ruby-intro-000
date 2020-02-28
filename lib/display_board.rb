044f9624a92170e1f74fcd9b5cfb2062fc04a700
Steven Williams <evilgeniusnyc@me.com>
2020-02-28 17:41:20 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,26 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]]
+
+
+#OUTPUT BOARD
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+puts "#{cell one[0]} | #cell 2[1]} | #{cell three[2]} 
+puts #{[3]}", #{[4]}", #{[5]}
+puts #{cell_four[6} | #{cell_five[7]} | #{cell_six[8]} 
+puts #{[9]}", #{[10]}", #{[11]}
+puts #{cell_seven[12]} | #{cell_eight[13]} | #{cell_nine[14]}"
+end
+
+
+
+
